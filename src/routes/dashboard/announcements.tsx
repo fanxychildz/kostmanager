ad9a3a8296cc2e340e43b0978d032d28c3f6c57f
fanxychildz <fanxychild1204@gmail.com>
2026-06-12 04:45:49 +0700

    fix: tambah import selectCache yang hilang di halaman pengumuman

--- src/routes/dashboard/announcements.tsx
+++ src/routes/dashboard/announcements.tsx
@@ -26,6 +26,7 @@ import {
 import { formatDate } from '~/lib/utils'
 import { api } from '~/lib/api'
 import { useQuery } from '~/lib/hooks'
+import { selectCache } from './_cache'
 
 export const Route = createFileRoute('/dashboard/announcements')({
   component: AnnouncementsPage,
